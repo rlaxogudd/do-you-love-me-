ad0d18e745d1d12c945733576b3981c3017aa15f
Ayşenur TURKARSLAN <159587158+rlaxogudd@users.noreply.github.com>
2025-03-22 21:43:52 +0300

    Add files via upload

---
+++ do-u-love-me.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('.container');
+const question = document.querySelector('.question');
+const yesButton = document.querySelector('.yes-button');
+const noButton = document.querySelector('.no-button');
+const catMeme = document.querySelector('.cat-image');
+
+const containerRect = container.getBoundingClientRect();
+const noButtonRect = noButton.getBoundingClientRect(); 
+
+yesButton.addEventListener('click', () => {
+  question.innerHTML= 'I love you too!!! 💖🥹💕';
+});
+
+noButton.addEventListener('mouseover', () => {
+  const i =Math.floor(Math.random() * (containerRect.width - noButtonRect.width)) +1;
+  const j =Math.floor(Math.random() * (containerRect.height - noButtonRect.height)) +1;
+
+  noButton.style.left = i + 'px';
+  noButton.style.top = j + 'px';
+});
